29095a2c2a9f1cca169860ef6e6485e23d8650cc
Eesha <eeshafatima0110@gmail.com>
2026-03-21 01:23:47 +0500

    Delete MILESTONE_2/Task 3.1.2/linearlayer.h

--- MILESTONE_2/Task 3.1.2/linearlayer.h
+++
@@ -1,9 +0,0 @@
-#ifndef LINEAR_LAYER_H
-#define LINEAR_LAYER_H
-
-#include <stddef.h>
-
-void linear_forward(const float *X, const float *W, const float *b,
-                    float *Y, size_t L, size_t Din, size_t Dout);
-
-#endif 
